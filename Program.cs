0a9503a75d39797103043a9242a16257017e6d8b
Mo Silvera <mo.leslie.silvera@gmail.com>
2020-05-14 09:23:11 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace companyEmployee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
