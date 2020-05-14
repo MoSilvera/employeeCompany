732ebfb51718f8be3ccf15b126b01b37325f3e53
Mo Silvera <mo.leslie.silvera@gmail.com>
2020-05-14 11:21:21 -0500

    employee company complete

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace companyEmployee
 {
@@ -6,7 +7,53 @@ namespace companyEmployee
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Company BigCorp = new Company("BigCorp", DateTime.Now);
+            Employee Jim = new Employee("Jimothee", "McJim", "Floor Washer");
+            Employee Dave = new Employee("Davina", "Good", "Drag superstar of the world");
+            Employee Drew = new Employee("Drewcilla", "Pointytooth", "Vampire in residence");
+            BigCorp.EmployeeList.Add(Jim);
+            BigCorp.EmployeeList.Add(Dave);
+            BigCorp.EmployeeList.Add(Drew);
+            BigCorp.EmployeeReport();
         }
+        public class Company
+        {
+
+            // Some readonly properties (let's talk about gets, baby)
+            public string Name { get; }
+            public DateTime CreatedOn { get; }
+
+            // Create a public property for holding a list of current employees
+            public List<Employee> EmployeeList {get; set;} = new List<Employee>();
+
+            public Company(string name, DateTime date){
+                Name = name;
+                CreatedOn = date;
+            }
+
+            public void EmployeeReport(){
+                 Console.WriteLine("***Employee Roster***");
+                foreach (Employee oneEmployee in EmployeeList)
+                {
+                    Console.WriteLine($"{oneEmployee.FirstName} {oneEmployee.LastName} is currently the {oneEmployee.Title}");
+                }
+            }
+
+        }
+        public class Employee
+        {
+            public string FirstName {get; set;}
+
+            public string LastName {get; set;}
+
+            public string Title {get; set;}
+
+            public Employee(string firstname, string lastname, string title){
+                    FirstName = firstname;
+                    LastName = lastname;
+                    Title = title;
+            }
+        }
+
     }
 }
